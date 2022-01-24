1c72c1b6f08c29e6f0b9dc2afd451a94f45d0ca2
Roshan <roshan_kapilasena@manulife.com>
2022-01-24 22:20:28 +0800

    initila commit

---
+++ src/main/java/com/manulife/sg/SgCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.manulife.sg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgCustomerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manulife/sg/SgCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manulife.sg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgCustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
